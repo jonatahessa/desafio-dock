73b99582edc7f82d7c0154c369c954df90385bc7
jonata.h.oliveira <ext_jhdoliveira@circlekna.com>
2022-03-22 18:23:52 -0300

    first commit

---
+++ src/test/java/br/com/desafio/dock/DockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.dock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
